3d9006be04314867d209db855e6aebeecf8ae7c4
Talha Azeem <>
2024-01-12 12:30:21 +0500

    graphs added

---
+++ src/components/Dashboard/Graphs.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactEcharts from "echarts-for-react";
+import * as echarts from "echarts/core";
+import { DatePicker } from "antd";
+const graphs = () => {
+  return <></>;
+};
+export default graphs;
