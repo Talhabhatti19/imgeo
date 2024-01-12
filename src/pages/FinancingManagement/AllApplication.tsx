33ec2a1106818ed8a62da2932772b7361b36ff62
usama saqib <usama.saqib@hyfatech.com>
2024-01-12 18:17:05 +0500

    dashboard bar & Table css added

--- src/pages/FinancingManagement/AllApplication.tsx
+++ src/pages/FinancingManagement/AllApplication.tsx
@@ -30,7 +30,7 @@ const AllApplication = () => {
   console.log(checkReduxState, "checkReduxState");
   return (
     <>
-      <div className="">
+      <div className="cs-table">
         <div className="d-flex align-items-center">
           <h2 className="col-6 fs-6 fw-bold mt-5">
             Financing Application List
