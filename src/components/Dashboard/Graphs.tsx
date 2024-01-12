3c8091d0ab82217e7946f3e910dc9c8defb52652
Talha Azeem <>
2024-01-12 14:35:54 +0500

    packages file chnaged

--- src/components/Dashboard/Graphs.tsx
+++ src/components/Dashboard/Graphs.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import ReactEcharts from "echarts-for-react";
-import * as echarts from "echarts/core";
+
 import { DatePicker } from "antd";
 const graphs = () => {
   return <></>;
