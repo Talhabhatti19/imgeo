ddae77219bac4a3e389fdada81963b8718007fba
usama saqib <usama.saqib@hyfatech.com>
2024-01-16 15:37:50 +0500

    dropdown & active & inactive tag added

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -3,13 +3,44 @@ import Graphs from "./Graphs";
 import OverView from "./OverView";
 import RecentApplication from "./RecentApplication";
 import NotificationBar from "./Notification";
+import { DatePicker } from "antd";
 
 function Dashboard() {
   return (
     <div>
-      <div className="d-flex align-items-center p-3">
-        <h2 className="col-6 fs-6 fw-bold">Overview</h2>
-        <div className="col-6 d-flex">from to</div>
+      <div className="col-lg-12 search-bar col-12 d-flex align-items-center">
+        <h2 className="col-lg-6 col-12 fs-6 fw-bold">Overview</h2>
+        <form
+          action="search-bar"
+          className="col-lg-6 col-12 d-flex justify-content-end pb-2"
+        >
+          <div className="d-flex justify-content-between ">
+            <div className="d-grid">
+              <label htmlFor="" className="label-theme">
+                From
+              </label>
+              <DatePicker
+                style={{
+                  width: "200px",
+                  height: "36px",
+                  marginRight: "10px",
+                }}
+              />
+            </div>
+
+            <div className="d-grid">
+              <label htmlFor="" className="label-theme">
+                To
+              </label>
+              <DatePicker
+                style={{
+                  width: "200px",
+                  height: "36px",
+                }}
+              />
+            </div>
+          </div>
+        </form>
       </div>
       <div className="col-12 d-flex">
         <div className="col-8">
