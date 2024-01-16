14066bf2f50f9b6fead069fb8c651ad70c51199c
usama saqib <usama.saqib@hyfatech.com>
2024-01-16 17:59:31 +0500

    table header dropdown & search bar added

--- src/pages/FinancingManagement/AllApplication.tsx
+++ src/pages/FinancingManagement/AllApplication.tsx
@@ -5,6 +5,8 @@ import { Images } from "../../components/Config/Images";
 import DataTable from "react-data-table-component";
 import { All_Application_Header } from "../../components/Config/TableHeaders";
 import TableView from "../../components/TableView/TableView";
+import { DatePicker, Select } from "antd";
+import { Dropdown } from "react-bootstrap";
 
 const data = [
   {
@@ -28,13 +30,113 @@ const data = [
 const AllApplication = () => {
   const checkReduxState = useSelector((state: RootState) => state.block.check);
   console.log(checkReduxState, "checkReduxState");
+  const actionSelect = [
+    { label: "Partners", img: Images.listIcon, Link: "partner" },
+    { label: "Settings", img: Images.settingIcon, Link: "setting" },
+    { label: "Documents", img: Images.settingIcon, Link: "document" },
+    { label: "Edit", img: Images.settingIcon, Link: "edit" },
+  ];
   return (
     <>
       <div className="cs-table">
-        <div className="d-flex align-items-center">
-          <h2 className="col-6 fs-6 fw-bold mt-5">
+        <div className="col-xl-12 col-12 d-flex align-items-center pb-3">
+          <h2 className="col-xl-2 col-12 fs-6 fw-bold">
             Financing Application List
           </h2>
+          <div className="col-xl-10 col-12 d-flex justify-content-end align-items-center">
+            <div className="d-grid pb-2 search-bar">
+              <label htmlFor="" className="label-theme">
+                Parent Status
+              </label>
+              <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                  Select
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  {actionSelect.map((item, index) => (
+                    <Dropdown.Item>
+                      <>
+                        <div className="d-flex">
+                          <div className="col-3">
+                            <input type="radio" />
+                          </div>
+
+                          {item.label}
+                        </div>
+                      </>
+                    </Dropdown.Item>
+                  ))}
+                </Dropdown.Menu>
+              </Dropdown>
+            </div>
+            <div className="d-grid pb-2 search-bar">
+              <label htmlFor="" className="label-theme">
+                Status
+              </label>
+              <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                  Select
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  {actionSelect.map((item, index) => (
+                    <Dropdown.Item>
+                      <>
+                        <div className="d-flex">
+                          <div className="col-3"></div>
+
+                          {item.label}
+                        </div>
+                      </>
+                    </Dropdown.Item>
+                  ))}
+                </Dropdown.Menu>
+              </Dropdown>
+            </div>
+            <div className="d-grid pb-2 search-bar">
+              <label htmlFor="" className="label-theme">
+                Partner
+              </label>
+              <Select style={{ width: "130px" }}>
+                <option value="1">Test</option>
+                <option value="1">Test</option>
+                <option value="1">Test</option>
+              </Select>
+            </div>
+            <form
+              action=""
+              className=" d-flex justify-content-end pb-2 search-bar"
+            >
+              <div className="d-flex justify-content-between ">
+                <div className="d-grid">
+                  <label htmlFor="" className="label-theme">
+                    From
+                  </label>
+                  <DatePicker
+                    style={{
+                      width: "200px",
+                      height: "36px",
+                      marginRight: "10px",
+                    }}
+                  />
+                </div>
+
+                <div className="d-grid">
+                  <label htmlFor="" className="label-theme">
+                    To
+                  </label>
+                  <DatePicker
+                    style={{
+                      width: "200px",
+                      height: "36px",
+                    }}
+                  />
+                </div>
+              </div>
+            </form>
+            <div className="theme-btn mt-1">Export CSV</div>
+          </div>
         </div>
         <TableView header={All_Application_Header} data={data} />
       </div>
