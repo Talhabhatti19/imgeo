d68192dcb7fd33cf18e7b71bd82af31231ac0e71
Talha Azeem <>
2024-02-12 15:01:05 +0500

    data structure changed into json and side bar text change

--- src/components/Dashboard/Cards.tsx
+++ src/components/Dashboard/Cards.tsx
@@ -3,10 +3,7 @@ import React from "react";
 const Cards = (props: any) => {
   return (
     <div className="col-md-3">
-      <div
-        className="card card-flex"
-        style={{ backgroundColor: props.backgroundColor }}
-      >
+      <div className="card card-flex">
         <img src={props.icon} alt={props.title} />
         <div className="card-body w-100">
           <div className="card-title" style={{ fontSize: "12px" }}>
