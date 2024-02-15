5a760848b7d677235c60d979c831b159a45c71e7
Arslan Kibria <ArslanKibria>
2024-02-15 18:10:50 +0500

    compliance dashboard mobile responsive

--- src/components/ComplianceDashboard/ComplianceDashboard.tsx
+++ src/components/ComplianceDashboard/ComplianceDashboard.tsx
@@ -26,9 +26,9 @@ function ComplianceDashboard() {
     compilanceData?.financeOverview &&
     compilanceData?.financeOverview.map((item: any, index: any) => (
       <div key={index} className="view-chart mb-4">
-        <div className="chart-header d-flex justify-content-between align-items-center">
+        <div className="chart-header d-flex mobile-responsive-chart justify-content-between align-items-center">
           <h3 className="welcome-heading">{item.title}</h3>
-          <div className="d-flex align-items-center">
+          <div className="d-flex mobile-responsive-chart-filter col-xl-9 col-12 align-items-center justify-content-end">
             <div className="d-grid">
               <label htmlFor="" className="label-theme">
                 From
@@ -65,8 +65,33 @@ function ComplianceDashboard() {
   const renderedBarOverview =
     compilanceData?.barchart &&
     compilanceData?.barchart.map((item: any, index: any) => (
-      <div key={index} className="col-6 p-2">
-        <h2 className="col-12 fs-6 fw-bold mt-5">{item.title}</h2>
+      <div key={index} className="col-md-6 col-12 p-2">
+        <div className="chart-header d-flex mobile-responsive-chart justify-content-between align-items-center pb-2">
+          <h3 className="welcome-heading filter-padding-responsive">
+            {item.title}
+          </h3>
+          <div className="d-flex mobile-responsive-chart-filter col-xl-9 col-12 align-items-center justify-content-end">
+            <div className="d-grid filter-padding-responsive">
+              <div style={{ marginRight: "15px" }}>
+                <Select style={{ width: "120px" }}>
+                  <option value="1">Test</option>
+                  <option value="1">Test</option>
+                  <option value="1">Test</option>
+                </Select>
+              </div>
+            </div>
+            <div className="d-grid">
+              <div style={{ marginRight: "15px" }}>
+                <Select style={{ width: "120px" }}>
+                  <option value="1">Test</option>
+                  <option value="1">Test</option>
+                  <option value="1">Test</option>
+                </Select>
+              </div>
+            </div>
+          </div>
+        </div>
+
         {item.child.map((chart: any, chartIndex: any) => (
           <div className="view-chart" key={chartIndex}>
             <Graphs option={chart} />
@@ -91,7 +116,10 @@ function ComplianceDashboard() {
             <div className="row">
               {compilanceData?.cards &&
                 compilanceData?.cards.map((card: any, index: any) => (
-                  <div className="col-md-3" key={index}>
+                  <div
+                    className="col-xl-3 col-sm-12 col-lg-4 col-md-6"
+                    key={index}
+                  >
                     <div className="card">
                       <img
                         src={card.icon ? card.icon : Images.Cash}
@@ -99,7 +127,7 @@ function ComplianceDashboard() {
                       />
                       <div className="card-body">
                         <div
-                          className="card-title"
+                          className="card-title h-auto"
                           style={{ fontSize: "12px" }}
                         >
                           {card.title}
@@ -115,7 +143,7 @@ function ComplianceDashboard() {
           </div>
         </div>
         <div>{renderedFinanceOverview}</div>
-        <div className="col-12 d-flex">
+        {/* <div className="col-12 d-flex">
           <div className="col-6">
             <h2
               className="col-12 fs-6 fw-bold mt-5"
@@ -132,25 +160,10 @@ function ComplianceDashboard() {
               >
                 Pie Chart
               </h2>
-              <h2 className="col-6 d-flex justify-content-end fs-6 fw-bold mt-5">
-                <div style={{ marginRight: "15px" }}>
-                  <Select style={{ width: "120px" }}>
-                    <option value="1">Test</option>
-                    <option value="1">Test</option>
-                    <option value="1">Test</option>
-                  </Select>
-                </div>
-                <div style={{ marginRight: "15px" }}>
-                  <Select style={{ width: "100px" }}>
-                    <option value="1">Test</option>
-                    <option value="1">Test</option>
-                    <option value="1">Test</option>
-                  </Select>
-                </div>
-              </h2>
+             
             </div>
           </div>
-        </div>
+        </div> */}
 
         <div className="d-flex flex-wrap col-12"> {renderedBarOverview}</div>
         <RecentApplication
