04adb9c4d986389764075908e5dcc94bca9f5dfe
ArslanKibria <ArslanKibria>
2024-02-28 15:35:47 +0500

    translator library implemented and homepage convertable into arabic

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,22 @@ import { router } from "./Routes/path";
 import { ThemeProvider } from "styled-components";
 import { theme } from "antd";
 import { Toaster } from "react-hot-toast";
+import { I18nextProvider } from "react-i18next";
+import i18n from "./components/i18n";
 
 const App = () => {
   return (
     <>
       <Toaster position="top-center" reverseOrder={false} />
-      <Provider store={store}>
-        <ThemeProvider theme={theme}>
-          <PersistGate persistor={persistor}>
-            <RouterProvider router={router} />
-          </PersistGate>
-        </ThemeProvider>
-      </Provider>
+      <I18nextProvider i18n={i18n}>
+        <Provider store={store}>
+          <ThemeProvider theme={theme}>
+            <PersistGate persistor={persistor}>
+              <RouterProvider router={router} />
+            </PersistGate>
+          </ThemeProvider>
+        </Provider>
+      </I18nextProvider>
     </>
   );
 };
--- src/components/HomePageManagement/ManagementForm.tsx
+++ src/components/HomePageManagement/ManagementForm.tsx
@@ -1,23 +1,25 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Images } from "../Config/Images";
 import "react-responsive-modal/styles.css";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import { Modal } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
+import i18n, { updateUserTranslations } from "../i18n";
 
 const ManagementForm = () => {
   const fileInputRef = useRef<HTMLInputElement>(null);
+  const { t } = useTranslation();
   const [open, setOpen] = useState(false);
   const [formValue, setFormValue] = useState("");
-  const [buttonText, setButtonText] = useState("Apply For Financing");
+  const [buttonText, setButtonText] = useState(t(`apply`));
   const [phoneNumber, setPhoneNumber] = useState("810 - 111 -1810");
-  const [headingText, setHeadingText] = useState(
-    "A simple way to finance your business"
-  );
+  const [headingText, setHeadingText] = useState<any>(t(`heading`));
   const [isEditing, setIsEditing] = useState(false);
   const [paragraphContent, setParagraphContent] = useState(
     "Apply now for a decision in 60 seconds.."
   );
+
   const onOpenModal = () => setOpen(true);
   const onCloseModal = () => setOpen(false);
   const handleChange = () => {
@@ -35,12 +37,30 @@ const ManagementForm = () => {
       fileInputRef.current.click();
     }
   };
+  const changeLanguage = (lng: any) => {
+    i18n.changeLanguage(lng);
+  };
+  console.log(t, "tttt");
+  useEffect(() => {
+    updateUserTranslations("ar", {
+      dynamicWelcome: "Hello, dynamic world!",
+      dynamicGreeting: "Greetings from the user!",
+    });
+    i18n.init({
+      lng: i18n.language,
+      fallbackLng: "en",
+      interpolation: {
+        escapeValue: false,
+      },
+    });
+  }, []);
+  console.log(updateUserTranslations);
   return (
     <>
       <div className="m-0" style={{ backgroundColor: "#f6f6f6" }}>
         <div className="col-md-12">
           <button className="btn-theme" style={{ padding: "14px" }}>
-            Publish
+            {t(`publish`)}
           </button>
         </div>
         <div className="p-2 ms-2 me-1">
@@ -60,7 +80,7 @@ const ManagementForm = () => {
                 style={{ width: "220px" }}
                 onClick={openDocument}
               >
-                Change Background Image
+                {t(`imageText`)}
               </button>
               <input
                 type="file"
@@ -89,23 +109,25 @@ const ManagementForm = () => {
         </div>
         <div className="bg-image-container">
           <div className="dropdown d-flex justify-content-end sticky">
-            <button
-              className="btn btn-secondary dropdown-toggle "
-              type="button"
-              id="dropdownMenuButton1"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              Select Language
-            </button>
-            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+            <label>
+              <select
+                className="home-management-select p-3"
+                onChange={(e) => changeLanguage(e.target.value)}
+                value={i18n.language}
+              >
+                <option value="en">English</option>
+                <option value="ar">عربي</option>
+                {/* Add more languages as needed */}
+              </select>
+            </label>
+            {/* <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
               <li>
                 <a className="dropdown-item">English</a>
               </li>
               <li>
                 <a className="dropdown-item">عربي</a>
               </li>
-            </ul>
+            </ul> */}
           </div>
           <div className="ps-2 pe-2">
             <div>
@@ -195,7 +217,7 @@ const ManagementForm = () => {
                       className="btn-theme mt-3"
                       style={{ width: "80px", height: "40px" }}
                     >
-                      Apply
+                      {t(`applyB`)}
                     </button>
                   </div>
                 </div>
@@ -208,7 +230,7 @@ const ManagementForm = () => {
       </div>
       <div className="wrapper" style={{ backgroundColor: "white" }}>
         <div>
-          <h1 className="text-center fw-8">Why Choose Merchant Cash Advance</h1>
+          <h1 className="text-center fw-8">{t(`whyChoose`)}</h1>
         </div>
         <div className="row mt-5">
           <div className="col-md-4">
@@ -216,8 +238,8 @@ const ManagementForm = () => {
               <img src={Images.timerIcon} alt="" />
             </div>
             <div className="mt-4 text-center">
-              <h4 className="mt-3 mb-3">Fast Funding Decisions</h4>
-              <p className="mb-3">POS Financing Decision with in 24 hours</p>
+              <h4 className="mt-3 mb-3">{t(`fundingHead`)}</h4>
+              <p className="mb-3">{t(`fundingText`)}</p>
             </div>
           </div>
           <div className="col-md-4">
@@ -225,8 +247,8 @@ const ManagementForm = () => {
               <img src={Images.dollarIcon} alt="" />
             </div>
             <div className="mt-4 text-center">
-              <h4 className="mt-3 mb-3">No Orignation Fee</h4>
-              <p className="mb-3">Shariah complaint with no hidden charges</p>
+              <h4 className="mt-3 mb-3">{t(`orinationHead`)}</h4>
+              <p className="mb-3">{t(`orinationText`)}</p>
             </div>
           </div>
           <div className="col-md-4">
@@ -234,55 +256,39 @@ const ManagementForm = () => {
               <img src={Images.documentationIcon} alt="" />
             </div>
             <div className="mt-4 text-center ">
-              <h4 className="mt-3 mb-3">We Serve Most Industries</h4>
-              <p className="mb-3">
-                We are the pioneer in shariah complaint <br /> financing
-              </p>
+              <h4 className="mt-3 mb-3">{t(`serveHead`)}</h4>
+              <p className="mb-3">{t(`serveText`)}</p>
             </div>
           </div>
         </div>
       </div>
       <div className="padder">
         <div className="row justify-content-center line-height">
-          <h1 className="text-center">About Us</h1>
+          <h1 className="text-center">{t(`about`)}</h1>
           <div className="mt-5 col-10 text-center">
-            Tanmeya Capital is a pioneering sharia-compliant financing platform
-            offering a wide range of flexible and innovative solutions, with
-            commercial registration (1010337706) licensed by the Saudi Central
-            Bank No. 22/أ ش/201410 dated 11/19/1435 AH <br />
-            <div className="mt-4">
-              Established in 2012, the company, with capital 500,000,000 SR ,
-              has a head office in Riyadh and 16 branches located in 13 cities
-              across Kingdom, it specializes in providing flexible financial
-              leasing, small and medium enterprises financing, productive assets
-              and consumer financing, subject to control and supervision The
-              Central Bank of Saudi Arabia.
-            </div>
+            {t(`aboutText`)}
+            <br />
+            <div className="mt-4">{t(`aboutTextOne`)}</div>
           </div>
         </div>
       </div>
       <div className="d-flex structure wrapper">
         <div className="col-md-6">
-          <h1>POS Financing</h1>
-          <p className="fs-6 mb-4 line-height">
-            POS Financing is the fastest financing facility, It is a quick fix
-            for your business problems. This facility can be availed within 24
-            hours and can be utilised for 30 days. Below are the salient
-            features for POS Financing product
-          </p>
+          <h1>{t(`pos`)}</h1>
+          <p className="fs-6 mb-4 line-height">{t(`postext`)}</p>
           <div
             className="d-flex justify-content-start"
             style={{ color: "#004d72" }}
           >
             <ul className="line-height p-1 list-style">
-              <li className="pb-3">Shariah Compliance</li>
-              <li className="pb-3">Amount: Upto SR 150,000</li>
-              <li className="pb-3">Duration: One month financing</li>
-              <li className="pb-4">Approval within 24 hours</li>
+              <li className="pb-3">{t(`pos1`)}</li>
+              <li className="pb-3">{t(`pos2`)}</li>
+              <li className="pb-3">{t(`pos3`)}</li>
+              <li className="pb-4">{t(`pos4`)}</li>
             </ul>
           </div>
           <button className="btn-theme mt-3 ms-3" style={{ width: "100px" }}>
-            Apply
+            {t(`applyB`)}
           </button>
         </div>
         <div className="col-md-6 ">
@@ -291,17 +297,10 @@ const ManagementForm = () => {
       </div>
       <div className="padder">
         <div className="row">
-          <h1 className="text-center fs-1 bold">Why choose us?</h1>
+          <h1 className="text-center fs-1 bold">{t(`us`)}</h1>
           <div className="d-flex justify-content-center">
             <div className="mt-5 col-10 text-center line-height">
-              Within our values and the Islamic Shari’a spirit and principles,
-              we have started our journey in 2012. And throughout, we have been
-              inspired to deal with our clients and partners, and that’s how we
-              started. Tanmeya Capital launched its operations promoting its
-              financing services for individuals and SME’s in compliancy with
-              the Islamic Shari'ah principles, by providing a variety of
-              financing products that our individual and corporate clients
-              entail.
+              {t(`usText`)}
             </div>
           </div>
         </div>
@@ -320,12 +319,12 @@ const ManagementForm = () => {
                 className="text-white text-center mb-3 "
                 style={{ fontSize: "2.4rem" }}
               >
-                Whether you need SR 8000 or SR 250,000 We can help you!
+                {t(`need`)}
               </h2>
 
               <div className="col-md-12 d-flex mt-4 justify-content-center">
                 <button className="btn-theme" style={{ width: "240px" }}>
-                  change background image
+                  {t(`imageText`)}
                 </button>
               </div>
             </div>
